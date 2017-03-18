bddd0591d3dea8c977723c65d83479ab2e8aec7d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-03-18 11:02:00 -0300

    feat(question): add basic question service boilerplate

---
+++ src/services/question/question-model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// question-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const questionSchema = new Schema({
+  text: { type: String, required: true },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+const questionModel = mongoose.model('question', questionSchema);
+
+module.exports = questionModel;
