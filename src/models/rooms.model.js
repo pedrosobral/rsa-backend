5e92acfab5c1bf76eb61a224a3bcf7d82ab0402d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-18 15:42:55 -0300

    feat(students): add students to rooms

--- src/models/rooms.model.js
+++ src/models/rooms.model.js
@@ -4,9 +4,17 @@
 
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
+
+  const student = new mongooseClient.Schema({
+      id: { type: String, required: true },
+      name: { type: String, required: true },
+      password: { type: String, required: true },
+  });
+
   const rooms = new mongooseClient.Schema({
     name: { type: String, required: true },
     code: { type: String, required: true },
+    students: [student],
     online: { type: Boolean, default: false },
     peopleOnline: { type: Number, default: 0 },
     default: { type: Boolean, default: false },
