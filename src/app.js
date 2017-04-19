73a593f41d4bf8de1e08b563861bc0f3b00c0535
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-19 14:51:24 -0300

    feat(users): add users and authentication

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ const appHooks = require('./app.hooks');
 
 const mongodb = require('./mongodb');
 
+const authentication = require('./authentication');
+
 const app = feathers();
 
 // Load app configuration
@@ -39,6 +41,8 @@ app.configure(mongodb);
 app.configure(rest());
 app.configure(socketio());
 app.configure(require('./functions'));
+
+app.configure(authentication);
 
 // Set up our services (see `services/index.js`)
 app.configure(services);
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const users = require('./users/users.service.js');
+
 const labels = require('./labels/labels.service.js');
 
 const questions = require('./questions/questions.service.js');
@@ -10,6 +12,7 @@ const rooms = require('./rooms/rooms.service.js');
 
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
   app.configure(labels);
   app.configure(questions);
   app.configure(polls);
