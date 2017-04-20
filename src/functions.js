dbc455da5fb5fa60d8aaaeb94ed02cdac00e227d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-20 20:01:50 -0300

    feat(socket.io): handle students online

--- src/functions.js
+++ src/functions.js
@@ -1,59 +1,112 @@
-const socketio = require('feathers-socketio');
+'use strict';
 
-module.exports = socketio(function (io) {
+const socketio = require('feathers-socketio');
+const logger = require('winston');
+
+module.exports = socketio(function(io) {
   const app = this;
 
-  io.on('connection', function (socket) {
-    console.info('socket connect')
-    socket.on('enter room', function (code) {
-      if (!code) {
-        console.info('No code provided. Abort.');
+  /**
+   * Logout student from a given room
+   * @param  {string} room    room id
+   * @param  {string} student student id
+   */
+  const logout = (room, student) => {
+    logger.info('logout student', room, student);
+    app.service('rooms').patch(room._id, {
+      $set: {
+        'students.$.online': false
+      }
+    }, {
+      query: {
+        'students.id': student,
+      }
+    });
+  };
+
+  /**
+   * Increment people online on an anonymous room
+   * @param  {string} id    room id
+   * @param  {number} value value to increment
+   */
+  const inc = (id, value) => {
+    if (!id) return;
+    app.service('rooms').patch(id, {
+      $inc: {
+        peopleOnline: value
+      }
+    });
+  };
+
+  const clear = (id) => {
+    if (!id) return;
+    app.service('rooms').patch(id, {
+      'peopleOnline': 0
+    });
+  };
+
+  io.on('connection', function(socket) {
+    logger.info('socket connect');
+
+    socket.on('clear room', (data) => {
+      logger.info('broadcasting clear room event', data);
+      socket.broadcast.emit('clear room', {
+        room: data.room._id
+      });
+
+      // reset people online
+      // wait just a bit sockets inc
+      setTimeout(() => clear(data.room._id), 500);
+    });
+
+    socket.on('student enter room', (data) => {
+      logger.info('student enter room', data);
+      socket.room = data.room;
+      socket.student = data.student;
+    });
+
+    socket.on('student leave room', () => {
+      logger.info('student leave room', socket.room, socket.student);
+
+      logout(socket.room, socket.student);
+    });
+
+    socket.on('anonymous enter room', (data) => {
+      if (!data.room) {
+        logger.info('No code provided. Abort.');
         return;
-      };
-      console.info('Enter room with code', code);
+      }
+      logger.info('anonymous enter room with code', data.room);
 
-      app.service('rooms').find({
-          query: {
-            'code': code
-          }
-        })
-        .then((room) => {
-          if (!room.total) {
-            console.info('Room does not exist. Abort.');
-            return;
-          }
-          socket.room_id = room.data[0]._id;
+      socket.room = data.room;
+      socket.anonymous = true;
 
-          inc(socket.room_id, 1);
-        });
+      inc(socket.room._id, 1);
     });
 
-    socket.on('leave room', (room) => {
-      inc(socket.room_id, -1);
-      delete socket.room_id;
+    socket.on('anonymous leave room', () => {
+      inc(socket.room._id, -1);
+      delete socket.room;
     });
 
-    socket.on('disconnect', function () {
-      console.info('socket disconnect');
-      if (!socket.room_id) {
-        console.info('socket without room code. Abort.');
+    socket.on('disconnect', function() {
+      logger.info('socket disconnect');
+      if (!socket.room) {
+        logger.info('socket without room code. Abort.');
         return;
       }
 
-      app.service('rooms').get(socket.room_id)
-        .then((room) => {
-          console.info('disconnect room', room);
-          inc(socket.room_id, -1);
-        });
-    });
+      // logout student user
+      if (socket.student) {
+        logger.info('socket with student connected...');
+        logout(socket.room, socket.student);
+      }
 
-    const inc = (id, value) => {
-      if (!id) return;
-      app.service('rooms').patch(id, {
-        $inc: {
-          peopleOnline: value
-        }
-      });
-    }
+      // logout anonymous user
+      if (socket.anonymous) {
+        logger.info('anonymous disconnect room');
+        inc(socket.room._id, -1);
+      }
+    });
   });
 });
