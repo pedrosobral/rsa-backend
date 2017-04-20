509b729eab94220ecda83578f3e23e1195e5dc03
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-20 08:20:40 -0300

    feat(users): add name

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,17 +1,15 @@
 'use strict';
 
 // users-model.js - A mongoose model
-//
-// See http://mongoosejs.com/docs/models.html
-// for more of what you can do here.
+
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
+
+    name: {type: String, required: true},
     email: {type: String, unique: true},
     password: { type: String },
-  
-  
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
