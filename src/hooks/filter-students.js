80cdac41571739ac85280675bd15f33811e7f00d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-22 15:02:20 -0300

    fix(filter-students): check room

--- src/hooks/filter-students.js
+++ src/hooks/filter-students.js
@@ -15,7 +15,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     if (hook.params.query['students.id']) {
       const studentId = hook.params.query['students.id'];
       const room = hook.result && hook.result.data && hook.result.data[0];
-      room.students = room.students && room.students.find(x => x.id === studentId);
+      room.students = room && room.students && room.students.find(x => x.id === studentId);
       logger.info('Hook::after::find::rooms::(studenId, room)', studentId, room);
     }
 
