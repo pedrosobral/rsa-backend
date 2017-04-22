b3dfa3ad4595a790c572e11232935a6e69173cb0
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-22 08:20:19 -0300

    feat(attendance): wip model

--- src/models/attendance.model.js
+++ src/models/attendance.model.js
@@ -1,13 +1,14 @@
 'use strict';
 
 // attendance-model.js - A mongoose model
-// 
-// See http://mongoosejs.com/docs/models.html
-// for more of what you can do here.
+
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const attendance = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    code: { type: String, required: true },
+    room: '',
+    students: [],
+    online: { type: Boolean, required: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
