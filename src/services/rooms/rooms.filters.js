2a2fa18e81e12b9971f539eda91493b3dba69814
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-23 17:21:42 -0300

    feat(rooms): filter patched on room to connected users

--- src/services/rooms/rooms.filters.js
+++ src/services/rooms/rooms.filters.js
@@ -1,6 +1,14 @@
 /* eslint no-console: 1 */
-console.warn('You are using the default filter for the rooms service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'); // eslint-disable-line no-console
 
-module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  return data;
+module.exports = {
+  patched: [
+    function (data, connection) {
+
+      if (!connection.user) {
+        return false;
+      }
+
+      return data;
+    }
+  ],
 };
