b2341218724dc7481626ec4e8d3a33c065307d3a
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-23 19:22:53 -0300

    feat(heroku): heroku setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 const logger = require('winston');
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
--- src/mongodb.js
+++ src/mongodb.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 module.exports = function () {
   const app = this;
 
-  mongoose.connect(app.get('mongodb'));
+  // mongoose.connect(app.get('mongodb'));
+  mongoose.connect('mongodb://sobral:0*0thEEnd@cluster0-shard-00-00-ay1sb.mongodb.net:27017,cluster0-shard-00-01-ay1sb.mongodb.net:27017,cluster0-shard-00-02-ay1sb.mongodb.net:27017/rsa?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
