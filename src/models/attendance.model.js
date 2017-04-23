c95112b68679961e3a398419408b4bc86e910687
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-23 09:17:21 -0300

    feat(attendance): add room name

--- src/models/attendance.model.js
+++ src/models/attendance.model.js
@@ -6,7 +6,8 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const attendance = new mongooseClient.Schema({
     code: { type: String, required: true },
-    room: '',
+    room: { type: String, required: true },
+    name: { type: String, required: true },
     students: [],
     online: { type: Boolean, required: false },
     createdAt: { type: Date, default: Date.now },
