839a42bb5bb9233fd96ca048be5fae300a80a91e
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-24 21:17:11 -0300

    feat(mongodb): add mongo atlas url to config

--- src/mongodb.js
+++ src/mongodb.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose');
 module.exports = function () {
   const app = this;
 
-  // mongoose.connect(app.get('mongodb'));
-  mongoose.connect('mongodb://sobral:0*0thEEnd@cluster0-shard-00-00-ay1sb.mongodb.net:27017,cluster0-shard-00-01-ay1sb.mongodb.net:27017,cluster0-shard-00-02-ay1sb.mongodb.net:27017/rsa?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+  mongoose.connect(app.get('mongodbAtlas'));
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
