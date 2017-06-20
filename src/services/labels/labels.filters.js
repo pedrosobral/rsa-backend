c493fc53a2ef7daf9e2f884c3f01fec1af8f9886
Pedro Henrique <pedrosobralxv@gmail.com>
2017-06-19 23:27:06 -0300

    fix(filter): comment out filter events for now

--- src/services/labels/labels.filters.js
+++ src/services/labels/labels.filters.js
@@ -1,9 +1,9 @@
 /* eslint no-console: 1 */
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  if (data.user !== connection.user._id) {
-    return false;
-  }
+  // if (data.user !== connection.user._id) {
+  //   return false;
+  // }
 
   return data;
 };
