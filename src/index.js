8dce7ad525be5e0df8941b01843452aa691f9ba4
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-04 07:26:50 -0300

    chore(index): port connection

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 
 const logger = require('winston');
 const app = require('./app');
-const port = process.env.PORT || app.get('port');
+// const port = process.env.PORT || app.get('port');
+const port = app.get('port');
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
