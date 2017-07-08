a746d874017316b37b95f24d8537fb099fa6de93
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-07 23:41:01 -0300

    feat(polls): if user only send to the owner

--- src/services/polls/polls.filters.js
+++ src/services/polls/polls.filters.js
@@ -1,6 +1,10 @@
 /* eslint no-console: 1 */
-console.warn('You are using the default filter for the polls service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'); // eslint-disable-line no-console
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
+  if (connection.user) {
+    if (connection.user._id.toString() !== data.user.toString()) {
+      return false;
+    }
+  }
   return data;
 };
