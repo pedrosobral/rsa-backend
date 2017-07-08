0b38c8a8aa4acc7cef7d66e759c987f011139f9e
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-07 22:24:05 -0300

    feat(rooms): filter to only the user

--- src/services/rooms/rooms.filters.js
+++ src/services/rooms/rooms.filters.js
@@ -1,10 +1,13 @@
 /* eslint no-console: 1 */
 
 module.exports = {
-  patched: [
-    function (data, connection) {
-
+  all: [
+    function(data, connection) {
       if (!connection.user) {
+        return false;
+      }
+
+      if (connection.user._id.toString() !== data.user.toString()) {
         return false;
       }
 
