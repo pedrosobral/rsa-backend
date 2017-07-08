74be9a19045748a9df2863ad5bf81cdc566f4091
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-08 08:23:58 -0300

    feat(questions): add filter to send events to just owner

--- src/services/questions/questions.filters.js
+++ src/services/questions/questions.filters.js
@@ -1,9 +1,11 @@
 /* eslint no-console: 1 */
 
-module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  // if (data.user != connection.user._id) {
-  //   return false;
-  // }
+module.exports = function(data, connection, hook) { // eslint-disable-line no-unused-vars
+  // only connected users
+  if (!connection.user) return;
 
+  if (data.user.toString() !== connection.user._id.toString()) {
+    return false;
+  }
   return data;
 };
