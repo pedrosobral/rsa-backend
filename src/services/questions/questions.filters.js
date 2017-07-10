fe4a3f506b9b455142f5053b56b84537876a6142
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-10 07:35:24 -0300

    revert filters

--- src/services/questions/questions.filters.js
+++ src/services/questions/questions.filters.js
@@ -2,10 +2,10 @@
 
 module.exports = function(data, connection, hook) { // eslint-disable-line no-unused-vars
   // only connected users
-  if (!connection.user) return;
-
-  if (data.user.toString() !== connection.user._id.toString()) {
-    return false;
-  }
+  // if (!connection.user) return;
+  //
+  // if (data.user.toString() !== connection.user._id.toString()) {
+  //   return false;
+  // }
   return data;
 };
