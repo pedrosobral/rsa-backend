5ff1a48f6e2fdc967efb9e0e2f53c1fbbb514b0d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-07-11 10:55:25 -0300

    fix(poll): add filter, now we have user info, so we can filter by user

--- src/services/polls/polls.filters.js
+++ src/services/polls/polls.filters.js
@@ -1,10 +1,10 @@
 /* eslint no-console: 1 */
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  // if (connection.user) {
-  //   if (connection.user._id.toString() !== data.user.toString()) {
-  //     return false;
-  //   }
-  // }
+  if (connection.user) {
+    if (connection.user._id.toString() !== data.user.toString()) {
+      return false;
+    }
+  }
   return data;
 };
